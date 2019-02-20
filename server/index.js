e447e2942e1fdf99647931acc35e9b41d015b67e
snowy <snowy@snowys-MacBook-Pro.local>
2019-02-19 21:28:41 -0800

    Add CORS headers to server

--- server/index.js
+++ server/index.js
@@ -10,6 +10,13 @@ const port = 3001;
 app.use(parse.json());
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get('/pictures/:homeId', (req, res) => {
   db.getAll(req.params.homeId, (err, data) => {
     if (err) {
