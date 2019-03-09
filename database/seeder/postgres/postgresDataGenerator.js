8772d90068a337b91977ee429441f5a766972cbc
Shelly <pxshelly@gmail.com>
2019-03-08 22:01:35 -0800

    Create database folder
      - Use to hold data generators and database schemas for both postgres and cassandra)

---
+++ database/seeder/postgres/postgresDataGenerator.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const faker = require('faker');
+const Stream = require('stream');
+const ws = fs.createWriteStream('./postgres.csv');
+
+// outer loop numberOfBatches
+  // inner loop batchSize
+    // create data with faker
+    // write data to file, wait for it to finish
+    // clear data
+  //
+//
+
+class GenerateData extends Stream.Readable {
+  constructor(opt) {
+    super(opt);
+    
+    this.houses = 10000000;
+    this.completedHouses = 0; 
+    this.count = 1;
+    this.buffer = '';
+  }
+
+  generateData(houseId) {
+    let randomPhotos = Math.floor(Math.random() * 6) + 5;
+    for (let i = 0; i < randomPhotos; i++) {
+      this.buffer += `${houseId},${faker.image.imageUrl()},${faker.commerce.productAdjective()}\n`
+
+    }
+  }
+
+  _read() {
+    const interval = this.completedHouses + 100;
+    if (this.completedHouses === this.houses) {
+      this.push(null);
+    } else {
+      for (let j = this.completedHouses; j < interval; j++) {
+        this.generateData(this.completedHouses);
+        this.completedHouses++;
+      } 
+      this.push(this.buffer);
+      this.buffer= '';
+    }
+  }
+}
+
+const rs = new GenerateData();
+rs.pipe(ws);
